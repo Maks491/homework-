be5b8e0e0435c9b6cd5e01738276bab968eb1a71
Maksim Zelenko <maksimzel876@gmail.com>
2025-01-16 18:15:34 +0300

    complited Lesson5

--- homework/src/Lesson_6/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_6;
-
-public class MyArrayDataException extends Exception {
-    public MyArrayDataException(String message) {
-        super(message);
-    }
-}
--- homework/src/Lesson_6/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package Lesson_6;
-
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException(String message){
-        super(message);
-    }
-}
